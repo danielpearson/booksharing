21bf2be0d4b808b2afbab8711abdca7fd6e9d6ff
ChelseyFrank <chelsey@zaarly.com>
2012-11-11 00:15:49 -0800

    Finish static pages & refactor

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,25 +4,43 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Booksharing'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Booksharing')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Booksharing | Home")
     end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Booksharing | Help")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Booksharing | About Us")
     end
   end
 end
