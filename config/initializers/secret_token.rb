2a3810047c1a72152f60aefac05749d7817311b9
Daniel Pearson <pears1dv@gmail.com>
2012-11-10 17:21:20 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booksharing::Application.config.secret_token = 'f6ac88c9798e2665d0f4d7e853633cfd8a6a0056838fd0c29ac79a6f262b2d0e120410232cb80fe207872455985dfbeb49180cec3eca7f3e2cfc21527a64ba8b'
